f4c3f745a687faa2efb9d9a8dd7169d35eab365f
Mike Zboray <michael.zboray@gmail.com>
2020-08-08 19:40:14 -0700

    add screenshotlib using cefsharp

---
+++ ScreenshotLib/CefThread.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Threading;
+using CefSharp;
+using CefSharp.OffScreen;
+
+namespace ScreenshotLib
+{
+    public sealed class CefThread
+    {
+        private readonly ManualResetEventSlim _initEvent, _shutdownEvent;
+
+        private readonly Thread _thread;
+
+        private CefThread()
+        {
+            _initEvent = new ManualResetEventSlim(false);
+            _shutdownEvent = new ManualResetEventSlim(false);
+            _thread = new Thread(RunCef)
+            {
+                Name = "CefThread",
+                IsBackground = true
+            };
+        }
+
+        public static CefThread Instance { get; } = new CefThread();
+
+        public bool IsInitialized
+        {
+            get => _initEvent.IsSet;
+        }
+
+        public void Start()
+        {
+            if ((_thread.ThreadState & ThreadState.Unstarted) == ThreadState.Unstarted)
+            {
+                _thread.Start(null);
+                _initEvent.Wait();
+            }
+        }
+
+        public void Stop(bool waitForExit = false)
+        {
+            _shutdownEvent.Set();
+            if (waitForExit)
+            {
+                _thread.Join();
+            }
+        }
+
+        private void RunCef(object state)
+        {
+            try
+            {
+                CefInitializeWrapper();
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine($"Failed to initialize Cef:{e}");
+
+                if (!_initEvent.IsSet)
+                    _initEvent.Set();
+            }
+        }
+
+        private void CefInitializeWrapper()
+        {
+            try
+            {
+                var settings = new CefSettings()
+                {
+                    LogSeverity = LogSeverity.Disable
+                };
+
+                Cef.Initialize(settings, performDependencyCheck: true, browserProcessHandler: null);
+            }
+            finally
+            {
+                _initEvent.Set();
+            }
+
+            _shutdownEvent.Wait();
+            Cef.Shutdown();
+        }
+    }
+}
